3fc27044cfe49e143bcf33ad4a4700f16cc1fc5f
yunyan520 <lidianchao_520@163.com>
2017-07-11 17:56:46 +0800

    创建

---
+++ ButtonArrange/ButtonArrange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonArrange
+//
+//  Created by lidianchao on 2017/7/11.
+//  Copyright © 2017年 lidianchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ButtonArrange/ButtonArrange/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ButtonArrange
+//
+//  Created by lidianchao on 2017/7/11.
+//  Copyright © 2017年 lidianchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
